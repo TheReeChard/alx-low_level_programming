7113dd6e09c4f408c11b74a64eca95195503f4b3
The ReeChard <ebukammaduekwepaul@outlook.com>
2022-10-13 17:45:25 +0100

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'This program would print an information'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ReeChard[] = "\"Programming is like building a multilingual puzzle\n";
+
+	puts(ReeChard); /*writing the code to out*/
+
+	return (0);
+
+}
