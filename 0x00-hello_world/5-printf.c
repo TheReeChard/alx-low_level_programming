01b5c19d35f60e03799a6580f67f3b6190d01afc
The ReeChard <ebukammaduekwepaul@outlook.com>
2022-10-13 18:39:44 +0100

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Description: 'This program will print an information using printf'
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
+
