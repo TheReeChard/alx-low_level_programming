0efdb0beef9bfbbf6ee178fdc4a79c9b6cd97896
The ReeChard <ebukammaduekwepaul@outlook.com>
2022-10-14 16:15:22 +0100

    3 alphaBET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main -this betty is annoying
+ *
+ * Return: she is annoying ohhhhh
+ */
+int main(void)
+{
+	char ReeChard, ReeKyboo;
+
+	for (ReeChard = 'a'; ReeChard <= 'z'; ++ReeChard)
+		putchar(ReeChard);
+	for (ReeKyboo = 'A'; ReeKyboo <= 'Z'; ++ReeKyboo)
+		putchar(ReeKyboo);
+	putchar('\n');
+	return (0);
+}
