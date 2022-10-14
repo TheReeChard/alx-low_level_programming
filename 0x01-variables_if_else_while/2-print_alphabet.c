ee2aba677d02409cf0c34ea33049de4a58ef02c8
The ReeChard <ebukammaduekwepaul@outlook.com>
2022-10-14 16:04:34 +0100

    Fix

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+/**
+ * main -this betty is annoying
+ *
+ * Return: she is annoying ohhhhh
+ */
 int main(void) 
 {
 	char ReeChard;
+
 	for (ReeChard = 'a'; ReeChard <= 'z'; ++ReeChard)
 		putchar(ReeChard);
 	putchar('\n');
