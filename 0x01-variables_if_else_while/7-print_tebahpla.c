900c55a846d1b2f3f14be382ca85a9b3ee56aa89
The ReeChard <ebukammaduekwepaul@outlook.com>
2022-10-14 18:11:11 +0100

    7. Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main -this betty is annoying
+ *
+ * Return: she is annoying ohhhhh
+ */
+int main(void)
+{
+	char ReeChard;
+
+	for (ReeChard = 'z'; ReeChard >= 'a'; --ReeChard)
+		putchar(ReeChard);
+	putchar('\n');
+	return (0);
+}
