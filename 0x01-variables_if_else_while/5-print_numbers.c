37665b77c45fb70be5f202c482c0e12e916a1e2c
The ReeChard <ebukammaduekwepaul@outlook.com>
2022-10-14 17:36:37 +0100

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main -this betty is annoying
+ *
+ * Return: she is annoying ohhhhh
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; ++num)
+		printf("%d",num);
+	printf("\n");
+	return (0);
+}
