8b839de38a73235c2ae72a579a616f3cc24714a8
The ReeChard <ebukammaduekwepaul@outlook.com>
2022-10-14 17:53:47 +0100

    6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main -this betty is annoying
+ *
+ * Return: she is annoying ohhhhh
+ */
+int main(void)
+{
+	int ReeChard;
+
+	for (ReeChard = '0'; ReeChard <= '9'; ++ReeChard)
+		putchar(ReeChard);
+	putchar('\n');
+	return (0);
+}
