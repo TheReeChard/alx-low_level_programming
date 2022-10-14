d262621e2a0d4a33150a9761cddaa2bd120bb6fe
The ReeChard <ebukammaduekwepaul@outlook.com>
2022-10-14 21:35:48 +0100

    fix

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main -this betty is annoying
+ *
+ * Return: she is annoying ohhhhh
+ */
+int main(void)
+{
+	int n;
+	char ch;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(ReeChard);
+	}
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
