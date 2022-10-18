f8e280652e688ce85454a325c52be5ed14436f6d
Ebuka Mmaduekwe <113597608+TheReeChard@users.noreply.github.com>
2022-10-18 22:44:13 +0100

    Create 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - this protoype will add two int and return result
+ * @a: int a
+ * @b: int b
+ * Return: sum of int a and int b
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+
+	return (sum);
+}
