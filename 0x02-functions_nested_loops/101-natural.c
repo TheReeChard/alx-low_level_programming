cfcb3db6f7e31488189a615e1b4a5b2f594a84a4
Ebuka Mmaduekwe <113597608+TheReeChard@users.noreply.github.com>
2022-10-18 23:21:58 +0100

    Create 101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h>
+
+/**
+ * main - prints the sum of all the multiples in question 13
+ * 
+ * Return: Always 0
+ */
+int main(void)
+{
+	unsigned long int a, b, total;
+	int i;
+
+	a = 0;
+	b = 0;
+	total = 0;
+
+	for (i = 0; i < 1024; ++i)
+	{
+		if ((i % 3) == 0)
+		{
+			a = a + i;
+		} else if ((i % 5) == 0)
+		{
+			b = b + i;
+		}
+	}
+	total = a + b;
+	printf("%lu\n", total);
+	return (0);
+}
