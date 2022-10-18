3b1875d5f5576e11d041019cec9b9401107bb3e5
The ReeChard <ebukammaduekwepaul@outlook.com>
2022-10-18 10:46:13 +0100

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entry point of the program
+ *
+ * Return: return (0) means success
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+__My Everpresent README__
