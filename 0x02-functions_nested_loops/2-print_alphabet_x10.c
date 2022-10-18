218db8b9cfc556bd351741d82e88e68847b2fae2
Ebuka Mmaduekwe <113597608+TheReeChard@users.noreply.github.com>
2022-10-18 17:17:03 +0100

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,11 +8,11 @@ void print_alphabet_x10(void)
 	char ReeChard;
 	char count;
 	while (count < 10)
+		
 	{
 		for (ReeChard = 'a'; ReeChard <= 'z'; ++ReeChard)
 		putchar(ReeChard);
 	putchar('\n');
 		count++;
 	}
-	
 }
