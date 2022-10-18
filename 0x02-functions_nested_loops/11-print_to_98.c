ca36703ab1e78f59514e097d9f475b36197b4461
Ebuka Mmaduekwe <113597608+TheReeChard@users.noreply.github.com>
2022-10-18 22:53:05 +0100

    Create 11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - prints natural number
+ * @n: given int
+ * natural numbers
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	for (i = n;  <= 98; ++i)
+    printf("%d, ", i);
+  print("\n");
+	return (0);
+}
