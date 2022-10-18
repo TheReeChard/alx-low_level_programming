9073d6acf63688afc69db8eb4f1691d6019c78b3
Ebuka Mmaduekwe <113597608+TheReeChard@users.noreply.github.com>
2022-10-18 17:16:11 +0100

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,13 +1,12 @@
 #include "stdio.h"
 /**
- * print_alphabet() - will print alphabets
+ * print_alphabet_x10 - will print alphabets
  *
  */
 void print_alphabet_x10(void)
 {
 	char ReeChard;
 	char count;
-	
 	while (count < 10)
 	{
 		for (ReeChard = 'a'; ReeChard <= 'z'; ++ReeChard)
