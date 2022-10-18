1789e4802e300ca0897d7fb85ca39d5dc2f991cd
The ReeChard <ebukammaduekwepaul@outlook.com>
2022-10-18 20:21:21 +0100

    task 7

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * _abs - gets absolute value
+ * @n: the int to be checked
+ * Return: valu to be returned
+ */
+int _abs(int n)
+{
+	if (n>=0)
+	{
+		return (n);
+	}
+	return (-n);
