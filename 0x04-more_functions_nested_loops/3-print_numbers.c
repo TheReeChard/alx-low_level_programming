3b9231e3a5eedd11daeae470d9ea399f6a3770cd
The ReeChard <ebukammaduekwepaul@outlook.com>
2022-10-20 18:42:59 +0100

    TASK 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers, from 0 to 9
+ * followed by a new line
+ *
+ * Return: return the numbers
+ */
+void print_numbers(void)
+{
+	char c;
+
+	for (c = 48; c <= 57; ++c)
+		_putchar(c);
+	_putchar('\n');
+}
