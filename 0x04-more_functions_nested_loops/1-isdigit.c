3ecc54c7b7eca78f172133dbfd87a60cec6df578
The ReeChard <ebukammaduekwepaul@outlook.com>
2022-10-20 15:13:35 +0100

    TASK 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isdigit - This checks if the character is an uppercase
+ *
+ * @c: Character to be checked
+ *
+ * Return: 1 if c is a digit else returns 0
+ *
+ */
+int _isdigit(int c)
+{
+	return (c>= '0' && c <= '9');
+}
