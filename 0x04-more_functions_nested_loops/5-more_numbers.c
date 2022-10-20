690a3d4379696d5591b7ec156ac7d09db5c13848
The ReeChard <ebukammaduekwepaul@outlook.com>
2022-10-20 20:42:50 +0100

    TASK 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - function that prints numbers from 0 to 9
+ * except 2 and 4
+ *
+ * Return: returns void
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+	char num = 0;
+
+	while (num <= 14)
+	{
+		if (num >= 10)
+			_putchar(num / 10 + '0');
+	_putchar(num % 10 + '0');
+		num++;
+	}
+	_putchar('\n');
+	}
+}
