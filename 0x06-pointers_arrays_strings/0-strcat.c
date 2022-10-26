7d934a06028e5d3fd90b0c37c09a1c41ba34919c
The ReeChard <ebukammaduekwepaul@outlook.com>
2022-10-26 18:19:28 +0100

    TASK 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * _strcat - prototype that concatenates
+ * @dest: string that should be appended to
+ * @src: string to be appended
+ *
+ * Return: a pointer to the resulting string dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int a;
+	int b;
+
+	a = 0;
+	b = 0;
+
+	while (dest[a] != '\0')
+	{
+		a++;
+	}
+	while (src[b] != '\0')
+	{
+		dest[a] = src[b];
+		a++;
+		b++;
+	}
+
+	dest[i] = '\0';
+	return (dest);
+}
